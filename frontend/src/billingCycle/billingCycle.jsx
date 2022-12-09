a03bc3b7e2b2bcab9f06f945d3c050d179f69c0a
JailsonSilva <JailsonSilva@users.noreply.github.com>
2022-12-09 13:46:34 -0300

    Listagem de ciclos de pagamentos e ppequenos ajustes nos nomes
    Implementado listagem basica de ciclos de pagamentos e corrigido nomes de arquivos

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import { bindActionCreators } from "redux";
-import { connect, Connect } from "react-redux";
+import { connect } from "react-redux";
 
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
@@ -10,6 +10,7 @@ import TabsContent from "../common/tab/tabsContent";
 import TabHeader from "../common/tab/tabHeader";
 import TabContent from "../common/tab/tabContent";
 import {selectTab, showTabs} from "../common/tab/tabActions";
+import BillingCycleList from "./billingCycleList";
 
 class BillingCycle extends Component{
 
@@ -31,7 +32,7 @@ class BillingCycle extends Component{
                             <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
                         </TabsHeader>
                         <TabsContent>
-                            <TabContent id="tabList"><h1>Lista</h1></TabContent>
+                            <TabContent id="tabList"><BillingCycleList /></TabContent>
                             <TabContent id="tabCreate"><h1>Incluir</h1></TabContent>
                             <TabContent id="tabUpdate"><h1>Alterar</h1></TabContent>
                             <TabContent id="tabDelete"><h1>Excluir</h1></TabContent>
