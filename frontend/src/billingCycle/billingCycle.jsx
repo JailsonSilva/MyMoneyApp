357665d22ed704e879abdc1abf94ef1d819d620e
JailsonSilva <JailsonSilva@users.noreply.github.com>
2022-12-14 13:38:08 -0300

    implementando alteração de ciclo de pagamento
    Implementando tela para alteração de ciclo de pagamento já cadastrado

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -10,7 +10,11 @@ import TabsContent from "../common/tab/tabsContent";
 import TabHeader from "../common/tab/tabHeader";
 import TabContent from "../common/tab/tabContent";
 import {selectTab, showTabs} from "../common/tab/tabActions";
+import {create} from "./billingCycleActions";
+
 import BillingCycleList from "./billingCycleList";
+import BillingCycleForm from "./billingCycleForm";
+import { Form } from "redux-form";
 
 class BillingCycle extends Component{
 
@@ -33,8 +37,8 @@ class BillingCycle extends Component{
                         </TabsHeader>
                         <TabsContent>
                             <TabContent id="tabList"><BillingCycleList /></TabContent>
-                            <TabContent id="tabCreate"><h1>Incluir</h1></TabContent>
-                            <TabContent id="tabUpdate"><h1>Alterar</h1></TabContent>
+                            <TabContent id="tabCreate"><BillingCycleForm onSubmit={this.props.create}/></TabContent>
+                            <TabContent id="tabUpdate"><BillingCycleForm /></TabContent>
                             <TabContent id="tabDelete"><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
@@ -44,5 +48,5 @@ class BillingCycle extends Component{
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs, create}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
